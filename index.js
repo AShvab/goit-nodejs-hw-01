e365b86eefc2622f88d2c88c0776022e571cab62
AShvab <shvabovska@gmail.com>
2023-07-11 20:02:42 +0300

    finished

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+import contactsService from "./contacts.js";
+import yargs from "yargs";
 
-console.log("Hello world!");
+const { argv } = yargs(process.argv.slice(2));
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contactsService.removeContact(id);
+      console.log(deleteContact);
+      break;
+
+    case "update":
+      const updateContact = await contactsService.updateContactById(id, {
+        name,
+        email,
+        phone,
+      });
+      console.log(updateContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
